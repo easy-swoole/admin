b542f19797b09b494c8c568fbf5fa348a05182d2
如果的如果 <admin@fosuss.com>
2020-04-26 14:39:54 +0800

    up

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -5,7 +5,19 @@ namespace App\HttpController;
 
 
 
+use Swoole\Coroutine\Channel;
+use Swoole\Coroutine;
+
 class Index extends BaseController
 {
+    function test()
+    {
+        $channel = new Channel(8);
+        Coroutine::create(function ()use($channel){
+            Coroutine::sleep(2);
+            $channel->push(1);
+        });
 
+        $this->writeJson(200,$channel->pop());
+    }
 }
--- dev.php
+++ dev.php
@@ -8,14 +8,9 @@ return [
         'SOCK_TYPE' => SWOOLE_TCP,
         'RUN_MODEL' => SWOOLE_PROCESS,
         'SETTING' => [
-            'worker_num' => 8,
+            'worker_num' => 4,
             'reload_async' => true,
             'max_wait_time'=>3
-        ],
-        'TASK'=>[
-            'workerNum'=>4,
-            'maxRunningNum'=>128,
-            'timeout'=>15
         ]
     ],
     'TEMP_DIR' => null,
