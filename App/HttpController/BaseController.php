1c7372f1d0025d5c5f0a4b6b1f60c1c7719c5471
高彬展 <gaobinzhan@gmail.com>
2020-04-28 17:06:46 +0800

    成功及失败返回函数封装

--- App/HttpController/BaseController.php
+++ App/HttpController/BaseController.php
@@ -21,5 +21,17 @@ class BaseController extends AnnotationController
         return $validate->getVerifiedData();
     }
 
-    
+    public function success($result = [], $msg = 'OK')
+    {
+        $this->writeJson(Status::CODE_OK, $result, $msg);
+        return true;
+    }
+
+    public function error($msg = 'Fail', $code = '-1', $statusCode = Status::CODE_OK)
+    {
+        $this->writeJson($statusCode, [
+            'errCode' => $code
+        ], $msg);
+        return false;
+    }
 }
